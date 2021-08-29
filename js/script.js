a951262bb46de595fc02837341884dd106d599d1
Bigo Sena <luc.senamelo@gmail.com>
2021-08-29 13:05:43 -0300

    Adição de modal para aba de login.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+//modal(pop-up)
+const modalTriggerButtons = document.querySelectorAll("[data-modal-target]");
+const modals = document.querySelectorAll(".modal");
+const modalCloseButtons = document.querySelectorAll(".modal-close");
+
+modalTriggerButtons.forEach(elem => {
+  elem.addEventListener("click", event => toggleModal(event.currentTarget.getAttribute("data-modal-target")));
+});
+modalCloseButtons.forEach(elem => {
+  elem.addEventListener("click", event => toggleModal(event.currentTarget.closest(".modal").id));
+});
+modals.forEach(elem => {
+  elem.addEventListener("click", event => {
+    if(event.currentTarget === event.target) toggleModal(event.currentTarget.id);
+  });
+});
+
+// Close Modal with "Esc"...
+document.addEventListener("keydown", event => {
+  if(event.keyCode === 27 && document.querySelector(".modal.modal-show")) {
+    toggleModal(document.querySelector(".modal.modal-show").id);
+  }
+});
+
+function toggleModal(modalId) {
+  const modal = document.getElementById(modalId);
+
+  if(getComputedStyle(modal).display==="flex") { // alternatively: if(modal.classList.contains("modal-show"))
+    modal.classList.add("modal-hide");
+    setTimeout(() => {
+      document.body.style.overflow = "initial";
+      modal.classList.remove("modal-show", "modal-hide");
+      modal.style.display = "none";      
+    }, 200);
+  }
+  else {
+    document.body.style.overflow = "hidden";
+    modal.style.display = "flex";
+    modal.classList.add("modal-show");
+  }
+}
+
 // slideshow
 // criando variaveis e atrbuindo valor
 var slideIndex = 1;
@@ -26,3 +68,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
